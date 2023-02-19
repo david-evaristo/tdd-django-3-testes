d551b6eb68e5dec24a777271cc3f071a2ad163bb
davidevaristo91 <david.evaristo@altbank.co>
2023-02-19 00:36:38 -0300

    Primeiro commit

---
+++ animais/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from animais.models import Animal
+
+# Create your views here.
+
+def index(request):
+    context = {'caracteristicas' : Animal.objects.all()}
+    return render(request, 'index.html', context)
---
+++ setup/testes.py
@@ -0,0 +1,27 @@
+from django.test import LiveServerTestCase
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+class AnimaisTestCase(LiveServerTestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome('/usr/bin/chromedriver')
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_buscando_um_novo_animal(self):
+        '''Teste se usuario encontra um animal pesquisando'''
+        home_page = self.browser.get(self.live_server_url + '/')
+
+        
+        brand_element = self.browser.find_element(By.CSS_SELECTOR, '.navbar')
+        self.assertEqual('Busca Animais', brand_element.text)
+
+        busca_animal_input = self.browser.find_element(By.CSS_SELECTOR, 'input#buscar-animal')
+        self.assertEqual(busca_animal_input.get_attribute('placeholder'), 'Exemplo: leão, urso...')
+
+        busca_animal_input.send_keys('leão')
+        self.browser.find_element(By.CSS_SELECTOR, 'form button').click()
+
+        caracteristicas = self.browser.find_elements(By.CSS_SELECTOR, '.result-description')
+        self.assertGreater(len(caracteristicas), 3)
